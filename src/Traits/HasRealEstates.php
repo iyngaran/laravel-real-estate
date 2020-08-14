cfee23efe853cf7e348122f90f0a213249b6027c
Iyngaran Iyathurai <dev@iyngaran.info>
2020-08-14 11:24:33 +0530

    Added actions for status changes

--- src/Traits/HasRealEstates.php
+++ src/Traits/HasRealEstates.php
@@ -4,6 +4,7 @@
 namespace Iyngaran\RealEstate\Traits;
 
 
+use Iyngaran\RealEstate\Models\Contact;
 use Iyngaran\RealEstate\Models\RealEstatePost;
 
 class HasRealEstates
@@ -11,5 +12,10 @@ class HasRealEstates
     public function realEstates()
     {
         return $this->morphMany(RealEstatePost::class, 'owner');
+    }
+
+    public function contact()
+    {
+        return $this->morphOne(Contact::class, 'owner');
     }
 }
