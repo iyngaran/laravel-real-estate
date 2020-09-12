09007868baef06c0a6709cc1ad8e0698a33ac16f
Iyngaran Iyathurai <dev@iyngaran.info>
2020-09-12 12:43:32 +0530

    Added endpoints to upload image

--- src/Http/Controllers/Api/ImageUploadController.php
+++ src/Http/Controllers/Api/ImageUploadController.php
@@ -4,8 +4,8 @@
 namespace Iyngaran\RealEstate\Http\Controllers\Api;
 
 
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class ImageUploadController
@@ -13,17 +13,18 @@ class ImageUploadController
     public function upload(Request $request)
     {
         try {
-
             $file = $request->file('file');
-            $file_name = trim(str_replace( " ", "_", $file->getClientOriginalName()));
-            Storage::disk(storage_path('app/public/images/uploads/'))->put($file_name, File::get($file));
-
+            $file_name = trim(str_replace(" ", "_", $file->getClientOriginalName()));
+            Storage::disk('post-images')->put($file_name, File::get($file));
         } catch (\Exception $e) {
             return response(['errors' => ['message' => $e->getMessage()]], 404);
         }
-        return response()->json([
-                                    'path' => $file_name,
-                                    'message' => 'Successfully updated event media!'
-                                ], 200);
+        return response()->json(
+            [
+                'path' => $file_name,
+                'message' => 'Successfully updated event media!'
+            ],
+            200
+        );
     }
 }
