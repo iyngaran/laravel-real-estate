58444992ab2a77c6844bd22c586fdb141a1db8ea
Iyngaran Iyathurai <dev@iyngaran.info>
2020-12-05 19:12:36 +0530

    Added extra fileds

--- database/migrations/2020_12_04_000001_alter_realestate_posts_table.php
+++ database/migrations/2020_12_04_000001_alter_realestate_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRealestatePostsTable extends Migration
+class AlterRealestatePostsTable extends Migration
 {
     /**
      * Run the migrations.
